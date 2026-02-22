46b8dd9c6bc96a8359919ab917173d3962bfdead
Steven Beshensky <s.beshensky@gmail.com>
2026-02-22 12:49:53 -0600

    Remove compression middleware and related files
    - Remove compression middleware and its exports (compress.ts) and
      noop.ts
    - Stop applying compression in server.tsx and delete related files
    - Use dev flag to enable debug and return formatted HTML

--- src/server.tsx
+++ src/server.tsx
@@ -3,20 +3,18 @@ import { Hono } from 'hono';
 import { prerender } from 'preact-iso/prerender';
 import { env } from './iso/is-browser.js';
 import { Layout } from './server/layout.js';
-import { compression } from './server/middleware/compress.js';
 import { location } from './server/middleware/location.js';
 import { getMovie, getMovies } from './server/movies.js';
 
+const dev = process.env.NODE_ENV === 'development';
 dot.config({
-  debug: process.env.NODE_ENV === 'development',
+  debug: dev,
 });
 export const app = new Hono();
-const port = 8000;
 
 env.current = 'server';
 
 app
-  .use(compression())
   .get('/api/movies', async (c) => {
     const movies = await getMovies();
     return c.json(movies);
@@ -28,7 +26,13 @@ app
   .use(location)
   .get('*', async (c) => {
     const { html } = await prerender(<Layout context={c} />);
-    return c.html(`<!DOCTYPE html><html lang="en-US">${html}</html>`);
+
+    return c.html(
+      `<!doctype html>
+      <html lang="en-US">
+        ${html}
+      </html>`
+    );
   });
 
 export default app;
--- src/server/middleware/noop.ts
+++
@@ -1,5 +0,0 @@
-import { createMiddleware } from "hono/factory";
-
-export const noopMiddleware = createMiddleware(async (_c, next) => {
-  await next();
-});
