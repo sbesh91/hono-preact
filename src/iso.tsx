dcccfcfed68142328c86ba0d6b5f62a9fe47fa31
Steven Beshensky <s.beshensky@gmail.com>
2026-03-02 22:03:40 -0600

    Add view transitions and route-change handler

--- src/iso.tsx
+++ src/iso.tsx
@@ -1,4 +1,5 @@
 import type { FunctionComponent } from 'preact';
+import { flushSync } from 'preact/compat';
 import { lazy, Route, Router } from 'preact-iso';
 import NotFound from './pages/not-found.js';
 
@@ -6,9 +7,14 @@ const Home = lazy(() => import('./pages/home.js'));
 const Test = lazy(() => import('./pages/test.js'));
 const Movies = lazy(() => import('./pages/movies.js'));
 
+function onRouteChange() {
+  if (!document.startViewTransition) return;
+  document.startViewTransition(() => flushSync(() => {}));
+}
+
 export const Base: FunctionComponent = () => {
   return (
-    <Router>
+    <Router onRouteChange={onRouteChange}>
       <Route path="/" component={Home} />
       <Route path="/test" component={Test} />
       <Route path="/movies" component={Movies} />
