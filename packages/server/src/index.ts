63fbcf5d8510e3503cd8a2d41205373191438a48
Steven Beshensky <s.beshensky@gmail.com>
2026-05-09 19:14:00 -0500

    feat(server): routeServerModules adapter for handler dispatch

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,3 +3,4 @@ export { location } from './middleware/location.js';
 export { renderPage } from './render.js';
 export { actionsHandler } from './actions-handler.js';
 export { loadersHandler } from './loaders-handler.js';
+export { routeServerModules } from './route-server-modules.js';
