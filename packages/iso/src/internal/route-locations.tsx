55065abd6f19667d1b5cc10ac9edaa428fafd593
Steven Beshensky <s.beshensky@gmail.com>
2026-05-12 20:33:14 -0500

    feat(iso): RouteLocationsContext + provider

---
+++ packages/iso/src/internal/route-locations.tsx
@@ -0,0 +1,27 @@
+import { createContext, h } from 'preact';
+import type { ComponentChildren } from 'preact';
+import { useContext, useMemo } from 'preact/hooks';
+import type { RouteHook } from 'preact-iso';
+
+export const RouteLocationsContext = createContext<ReadonlyMap<string, RouteHook>>(
+  new Map()
+);
+
+export function RouteLocationsProvider({
+  moduleKey,
+  location,
+  children,
+}: {
+  moduleKey: string | undefined;
+  location: RouteHook;
+  children: ComponentChildren;
+}) {
+  const parent = useContext(RouteLocationsContext);
+  const next = useMemo(() => {
+    if (!moduleKey) return parent;
+    const m = new Map(parent);
+    m.set(moduleKey, location);
+    return m;
+  }, [parent, moduleKey, location]);
+  return h(RouteLocationsContext.Provider, { value: next }, children);
+}
