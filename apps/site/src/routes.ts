80c04c2cb47088c09ce35e67734df803c01d720b
Steven Beshensky <s.beshensky@gmail.com>
2026-05-15 10:19:06 -0500

    feat(demo): /demo index page + routes wired
    /demo lands with an explainer + link to /demo/projects. /demo/login
    hooked up. /demo/projects coming in the next commit.

--- apps/site/src/routes.ts
+++ apps/site/src/routes.ts
@@ -7,6 +7,13 @@ export default defineRoutes([
   { path: '/docs', view: docsView },
   { path: '/docs/*', view: docsView },
   {
+    path: '/demo',
+    children: [
+      { path: '', view: () => import('./pages/demo/index.js') },
+      { path: 'login', view: () => import('./pages/demo/login.js') },
+    ],
+  },
+  {
     path: '*',
     view: () => import('./pages/not-found.js'),
   },
